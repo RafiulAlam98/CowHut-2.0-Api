fed9d2ba7a5a2da408312ecc1c278f792877a0e7
M Rafiul Alam Tonmoy <mrafiul.alam7@gmail.com>
2023-07-14 14:34:45 +0600

    server and app setup complete

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import cors from 'cors'
+import express, { Application } from 'express'
+
+export const app: Application = express()
+
+app.use(cors)
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
