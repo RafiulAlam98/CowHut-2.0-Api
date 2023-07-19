50ede261e8ab09beb2ab2f0d653638310927d342
M Rafiul Alam Tonmoy <mrafiul.alam7@gmail.com>
2023-07-19 09:54:45 +0600

    DONE: Buyer  module (patch and delete)

--- src/app/modules/buyer/buyer.route.ts
+++ src/app/modules/buyer/buyer.route.ts
@@ -2,6 +2,9 @@ import express from 'express'
 import { BuyerController } from './buyer.controller'
 const router = express.Router()
 
+router.get('/:id', BuyerController.getSingleBuyer)
+router.patch('/:id', BuyerController.updateBuyer)
+router.delete('/:id', BuyerController.deleteBuyer)
 router.get('/', BuyerController.getAllBuyer)
 
 export const BuyerRoutes = {
